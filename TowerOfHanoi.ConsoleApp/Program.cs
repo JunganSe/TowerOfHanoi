a18af5f413eb46678fa58c11512ce55a83db952c
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-06-10 18:17:55 +0200

    Kod i Main så att spelet startar

--- TowerOfHanoi.ConsoleApp/Program.cs
+++ TowerOfHanoi.ConsoleApp/Program.cs
@@ -1,8 +1,14 @@
-﻿namespace TowerOfHanoi.ConsoleApp;
+﻿using TowerOfHanoi.Core;
+
+namespace TowerOfHanoi.ConsoleApp;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        var drawHandler = new DrawHandler();
+        var ioHandler = new IOHandler(drawHandler);
+        var game = new Game(ioHandler);
+        game.Run();
     }
 }
