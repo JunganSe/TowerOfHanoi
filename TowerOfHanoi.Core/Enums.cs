c2b247605c561644cbad46a2238def8e28a5accc
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-06-10 14:11:26 +0200

    Grund för hantering av user input

---
+++ TowerOfHanoi.Core/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace TowerOfHanoi.Core.Enums;
+
+public enum InputCommand
+{
+    None,
+    Stack1,
+    Stack2,
+    Stack3,
+    Restart,
+    Quit
+}
---
+++ TowerOfHanoi.Core/Interfaces/IIOHandler.cs
@@ -0,0 +1,8 @@
+﻿using TowerOfHanoi.Core.Enums;
+
+namespace TowerOfHanoi.Core.Interfaces;
+
+public interface IIOHandler
+{
+    public InputCommand GetInput();
+}
