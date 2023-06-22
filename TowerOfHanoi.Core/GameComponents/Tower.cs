87fbe159d88fd41f07ba58fdd7f57b3ac430f9f6
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-06-22 23:43:28 +0200

    Gjort om Tower till att ärva Stack<TowerPiece>

--- TowerOfHanoi.Core/GameComponents/Tower.cs
+++ TowerOfHanoi.Core/GameComponents/Tower.cs
@@ -1,23 +1,8 @@
 ﻿namespace TowerOfHanoi.Core.GameComponents;
 
-public class Tower
+public class Tower : Stack<TowerPiece>
 {
-    private readonly Stack<TowerPiece> _pieces = new();
-
     public string Name { get; init; } = "";
     public bool Highlight { get; set; } = false;
-    public int Count => _pieces.Count;
-    public bool HasContent => _pieces.Any();
-
-    public void Clear()
-        => _pieces.Clear();
-
-    public void Push(TowerPiece piece)
-        => _pieces.Push(piece);
-
-    public TowerPiece Pop()
-        => _pieces.Pop();
-
-    public TowerPiece Peek()
-        => _pieces.Peek();
+    public bool HasContent => this.Any();
 }
