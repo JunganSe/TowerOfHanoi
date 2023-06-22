b1dc5719453198b2d3e818f6cb2cd7d4140d14be
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-06-22 23:33:50 +0200

    DrawHandler.DrawBorders()

--- TowerOfHanoi.ConsoleApp/DrawHandler.cs
+++ TowerOfHanoi.ConsoleApp/DrawHandler.cs
@@ -16,7 +16,8 @@ public class DrawHandler
 
     public void DrawBorders()
     {
-        // Två rutor på höjden som sitter ihop.
+        DrawBox(PlayField.X, PlayField.Y, PlayField.Width, PlayField.Height);
+        DrawBox(MessageBox.X, MessageBox.Y, MessageBox.Width, MessageBox.Height);
     }
 
     public void DrawTowers(Towers towers)
@@ -37,5 +38,27 @@ public class DrawHandler
         string instruction = messages.Instruction.Truncate(maxWidth).PadRight(maxWidth);
         Console.SetCursorPosition(x, y + 1);
         Console.Write(instruction);
+    }
+
+
+
+    private void DrawBox(int x, int y, int width, int height)
+    {
+        string parts = "─│┌┐└┘";
+        string horizontalLine = new String(parts[0], width - 2);
+
+        Console.SetCursorPosition(x, y);
+        Console.Write(parts[2] + horizontalLine + parts[3]);
+
+        for (int i = 1; i < height - 1; i++)
+        {
+            Console.SetCursorPosition(x, y + i);
+            Console.Write(parts[1]);
+            Console.SetCursorPosition(x + width - 1, y + i);
+            Console.Write(parts[1]);
+        }
+
+        Console.SetCursorPosition(x, y + height - 1);
+        Console.Write(parts[4] + horizontalLine + parts[5]);
     }
 }
--- TowerOfHanoi.ConsoleApp/Program.cs
+++ TowerOfHanoi.ConsoleApp/Program.cs
@@ -6,8 +6,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var playField = new Rectangle(2, 2, 50, 20);
-        var messageBox = new Rectangle(2, 2, 50, 4);
+        var playField = new Rectangle(2, 1, 43, 12);
+        var messageBox = new Rectangle(2, 13, 43, 4);
         var drawHandler = new DrawHandler(playField, messageBox);
         var ioHandler = new IOHandler(drawHandler);
         var game = new Game(ioHandler);
