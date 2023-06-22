c8d7814359f1c536e0fb82e6a5ce6782bf7ef600
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-06-22 21:20:54 +0200

    Skapat extensions-del i core, med extension för att förkorta string

---
+++ TowerOfHanoi.Core/Extensions/CommonExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace TowerOfHanoi.Core.Extensions;
+
+public static class CommonExtensions
+{
+    public static string Truncate(this string value, int maxLength)
+        => (value.Length <= maxLength) ? value : value[..maxLength];
+}
