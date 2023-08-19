c2d673081cfd0b2a981bab14764d8488170f2f6c
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-08-19 23:19:11 +0200

    Skapat komponent: World

---
+++ TowerOfHanoi.Core/Components/World.cs
@@ -0,0 +1,17 @@
+﻿using TowerOfHanoi.Core.Enums;
+
+namespace TowerOfHanoi.Core.Components;
+
+public class World
+{
+    public GameState GameState { get; set; }
+    public Towers Towers { get; set; }
+    public Messages Messages { get; set; }
+
+    public World()
+    {
+        GameState = GameState.None;
+        Towers = new Towers();
+        Messages = new Messages();
+    }
+}
