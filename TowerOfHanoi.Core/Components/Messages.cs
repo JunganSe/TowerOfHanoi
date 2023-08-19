1de40013c4945d2a601eaa9a6b6412202d2eb857
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-08-19 23:03:38 +0200

    Skapat komponent: Messages

---
+++ TowerOfHanoi.Core/Components/Messages.cs
@@ -0,0 +1,13 @@
+﻿namespace TowerOfHanoi.Core.Components;
+
+public class Messages
+{
+    public string Instruction { get; set; } = "";
+    public string Status { get; set; } = "";
+
+    public void Clear()
+    {
+        Instruction = "";
+        Status = "";
+    }
+}
