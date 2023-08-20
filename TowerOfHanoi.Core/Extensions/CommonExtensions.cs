169b324520716b64c345db1624c97683f7d3f4f6
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-08-20 12:00:12 +0200

    Core: Extension på string för att padda båda sidor

--- TowerOfHanoi.Core/Extensions/CommonExtensions.cs
+++ TowerOfHanoi.Core/Extensions/CommonExtensions.cs
@@ -5,4 +5,14 @@ public static class CommonExtensions
     public static string Truncate(this string value, int maxLength)
         => (value.Length <= maxLength) 
             ? value : value[..maxLength];
+
+    public static string PadBoth(this string value, int totalWidth, char paddingChar = ' ')
+    {
+        int totalPaddingSize = totalWidth - value.Length;
+        int leftPaddingSize = totalPaddingSize / 2;
+        int rightPaddingSize = totalPaddingSize - leftPaddingSize;
+        string leftPadding = new string(paddingChar, leftPaddingSize);
+        string rightPadding = new string(paddingChar, rightPaddingSize);
+        return leftPadding + value + rightPadding;
+    }
 }
