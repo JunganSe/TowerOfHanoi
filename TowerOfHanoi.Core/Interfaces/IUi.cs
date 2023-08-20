22f063855e0e8c962172fd1a268ab0b170c34748
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-08-20 08:44:22 +0200

    Skapat interface för UI

---
+++ TowerOfHanoi.Core/Interfaces/IUi.cs
@@ -0,0 +1,11 @@
+﻿using TowerOfHanoi.Core.Components;
+using TowerOfHanoi.Core.Enums;
+
+namespace TowerOfHanoi.Core.Interfaces;
+
+public interface IUi
+{
+    public void Initialize();
+    public void Draw(World world);
+    public InputCommand GetInput();
+}
