64816d2ac92ed57ba8cf712a9099724fc9b59484
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-08-20 11:01:59 +0200

    ConsoleUi: Rita tornen

--- TowerOfHanoi.ConsoleUi/OutputHandler.cs
+++ TowerOfHanoi.ConsoleUi/OutputHandler.cs
@@ -8,11 +8,22 @@ internal class OutputHandler
 {
     private readonly Rectangle _playField;
     private readonly Rectangle _messageBox;
+    private readonly string[] _towerPieces;
 
     public OutputHandler()
     {
         _playField = new Rectangle(2, 1, 62, 12);
         _messageBox = new Rectangle(2, 13, 62, 4);
+        _towerPieces = new string[]
+            {
+                "      ==      ",
+                "     ====     ",
+                "    ======    ",
+                "   ========   ",
+                "  ==========  ",
+                " ============ ",
+                "==============",
+            };
     }
 
     public void ClearScreen()
@@ -28,7 +39,16 @@ internal class OutputHandler
 
     public void DrawTowers(Towers towers)
     {
-        throw new NotImplementedException();
+        int x = _playField.X + 2;
+        int y = _playField.Y + 9;
+
+        Console.SetCursorPosition(x, y);
+        Console.Write("~~~~~~~~~~~~~~~~~~  ~~~~~~~~~~~~~~~~~~  ~~~~~~~~~~~~~~~~~~");
+        Console.SetCursorPosition(x, y + 1);
+        Console.Write("        1                   2                   3         ");
+        DrawTower(towers.Left, x, y);
+        DrawTower(towers.Middle, x + 20, y);
+        DrawTower(towers.Right, x + 40, y);
     }
 
     public void DrawMessages(Messages messages)
@@ -66,5 +86,15 @@ internal class OutputHandler
 
         Console.SetCursorPosition(x, y + height - 1);
         Console.Write(parts[4] + horizontalLine + parts[5]);
+    }
+
+    private void DrawTower(Tower tower, int x, int y)
+    {
+        for (int i = 0; i < tower.Count; i++)
+        {
+            int size = tower.Reverse().ElementAt(i).Size;
+            Console.SetCursorPosition(x + 2, y - 1 - i);
+            Console.Write(_towerPieces[size - 1]);
+        }
     }
 }
