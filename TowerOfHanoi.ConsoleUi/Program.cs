3170bb3c3b74562d58d692c1c2f3aff58c853aab
Johan Ljungberg <89130153+Ljungberg1989@users.noreply.github.com>
2023-08-24 23:05:22 +0200

    ConsoleUi: Kod som startar spelet

--- TowerOfHanoi.ConsoleUi/Program.cs
+++ TowerOfHanoi.ConsoleUi/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace TowerOfHanoi.ConsoleUi;
+﻿using TowerOfHanoi.Core.Game;
+
+namespace TowerOfHanoi.ConsoleUi;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-
+        var ui = new Ui();
+        var gameController = new Controller(ui);
+        gameController.Run();
     }
 }
